45f973d2c7d0417fe7026c089d3f18f5632f97d8
cassidian1 <cassidian1@7458a0d9-5978-ed7a-2a9b-614d889ad30b>
2012-11-18 22:07:50 +0000

    adding inventory class

---
+++ virtual-pet-app/src/edu/gatech/virtual_pet_app/Inventory.java
@@ -0,0 +1,20 @@
+public class Inventory()
+{
+	ArrayList<Item> items;
+	
+	public Inventory()
+	{
+		items = new ArrayList<Item>();
+	}
+	
+	public Inventory(ArrayList<Item> items)
+	{
+		this.items = items;
+	}
+	
+	public void addItem(item)
+	{
+		items.add(item);
+	}
+	
+}
