580212f5537bcbad1d1d3c01598a37eb1c865d7a
cassidian1 <cassidian1@7458a0d9-5978-ed7a-2a9b-614d889ad30b>
2012-11-28 03:55:25 +0000

    changes in every class except illness. Inventory has been fully moved to user. Breed has been deleted. Inventory has been changed to 2 maps rather than a map in an array list. Removed redundant code in addItem().

--- trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/Breed.java
+++
@@ -1,5 +0,0 @@
-package edu.gatech.virtual_pet_app;
-
-public class Breed {
-
-}
--- trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/EventHandler.java
+++ trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/EventHandler.java
@@ -1,5 +1,15 @@
 package edu.gatech.virtual_pet_app;
 
-public class EventHandler{
+public class EventHandler
+{
+	private Pet pet;
+	public EventHandler(Pet pet)
+	{
+		this.pet = pet;
+	}
 	
+	public void feedEvent(int pre, int post )
+	{
+		
+	}
 }
