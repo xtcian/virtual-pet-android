2a1db53c703ac49a913f01b11d3bcb0eb8900f06
wangleshi1991@gmail.com <wangleshi1991@gmail.com@7458a0d9-5978-ed7a-2a9b-614d889ad30b>
2012-12-06 03:49:42 +0000

    illness

--- trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/Illness.java
+++ trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/Illness.java
@@ -1,13 +1,18 @@
 package edu.gatech.virtual_pet_app;
 
 public class Illness {
+	private String name;
 	private int healthImpact;
 	private int happinessImpact;
 
-	public Illness(int health, int happy) {
+	public Illness(String name,int health, int happy) {
+		this.name=name;
 		healthImpact = health;
 		happinessImpact = happy;
 
+	}
+	public String getName(){
+		return name;
 	}
 
 	public int getHealthImpact() {
