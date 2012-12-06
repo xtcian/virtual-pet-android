fccc56e94741ec93b32a6504ce0cdacd85dd42d6
wangleshi1991@gmail.com <wangleshi1991@gmail.com@7458a0d9-5978-ed7a-2a9b-614d889ad30b>
2012-12-06 02:41:50 +0000

    woot

--- trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/database/VPDHelper.java
+++ trunk/virtual-pet-app/src/edu/gatech/virtual_pet_app/database/VPDHelper.java
@@ -7,7 +7,6 @@ import android.util.Log;
 
 public class VPDHelper extends SQLiteOpenHelper {
 
-
 	// All Static variables
 	// Database Version
 	public static final int DATABASE_VERSION = 1;
@@ -28,13 +27,14 @@ public class VPDHelper extends SQLiteOpenHelper {
 	public static final String TABLE_JOB = "Job";
 
 	// User Table Columns names
-	public static final String USER_ID = "ID";
+	public static final String USER_ID = "_id";
 	public static final String USER_NAME = "Name";
 	public static final String USER_PASSWORD = "Password";
 	public static final String USER_MONEY = "Money";
+	public static final String USER_PET = "pet_id";
 
 	// Pet Table Columns names
-	public static final String PET_ID = "ID";
+	public static final String PET_ID = "pet_id";
 	public static final String PET_NAME = "Name";
 	public static final String PET_AGE = "Age";
 	public static final String PET_AGE_INC = "AgeIncrement";
@@ -44,100 +44,116 @@ public class VPDHelper extends SQLiteOpenHelper {
 	public static final String PET_HUNGER = "Hunger";
 	public static final String PET_SICKNESS = "Illness";
 	public static final String PET_MOOD = "Mood";
-	
+
 	// Illness Table Columns names
 	public static final String ILLNESS_NAME = "Name";
 	public static final String ILLNESS_HAIMPACT = "HappinessImpact";
 	public static final String ILLNESS_HEIMPACT = "HealthImpact";
-	public static final String ILLNESS_HUIMPACT = "HungerImpact";
-	public static final String ILLNESS_DESCRIPTION = "Description";
-	
+
 	// Mood Table Columns names
 	public static final String MOOD_TYPE = "Type";
 	public static final String MOOD_HAIMPACT = "HappinessImpact";
 	public static final String MOOD_DESCRIPTION = "Description";
-	
+
 	// RandEvent Table Columns names
-	public static final String REVENT_ID  = "ID";
+	public static final String REVENT_ID = "_id";
 	public static final String REVENT_HAIMPACT = "HappinessImpact";
 	public static final String REVENT_HEIMPACT = "HealthImpact";
 	public static final String REVENT_HUIMPACT = "HungerImpact";
 	public static final String REVENT_ILLNESS = "Illness";
 	public static final String REVENT_ITEM = "Item";
 	public static final String REVENT_DESCRIPTION = "Description";
-	
+
 	// Event Table Columns names
-	public static final String EVENT_PET_ID = "PetID";
-	public static final String EVENT_RAND_ID = "RandID";
-	public static final String EVENT_TIME = "Time";
-	
-	
+	public static final String EVENT_PET_ID = "pet_id";
+	public static final String EVENT_RAND_ID = "rand_id";
+	public static final String EVENT_TIME = "time";
+
 	// Interaction Table Columns names
-	public static final String INTERACTION_PET_ID= "PetID";
-	public static final String INTERACTION_TIME = "Time";
-	public static final String INTERACTION_POST_HAPPINESS= "PostHappiness";
-	public static final String INTERACTION_POST_HEALTH= "PostHealth";
-	public static final String INTERACTION_POST_HUNGER= "PostHunger";
-	
+	public static final String INTERACTION_PET_ID = "pet_id";
+	public static final String INTERACTION_TIME = "time";
+	public static final String INTERACTION_POST_HAPPINESS = "PostHappiness";
+	public static final String INTERACTION_POST_HEALTH = "PostHealth";
+	public static final String INTERACTION_POST_HUNGER = "PostHunger";
+
 	// Item Table Columns names
-	public static final String ITEM_ID = "ID";
+	public static final String ITEM_ID = "item_id";
 	public static final String ITEM_PRICE = "Price";
 	public static final String ITEM_DESCRIPTION = "Description";
-	public static final String ITEM_TYPE= "Type";
-	public static final String ITEM_IMPACT= "Impact";
+	public static final String ITEM_TYPE = "Type";
+	public static final String ITEM_IMPACT = "Impact";
 	public static final String ITEM_ATTRIBUTE = "Attribute";
-	
+
 	// Owned Table Columns names
-	public static final String OWNED_USER_ID = "UserID";
-	public static final String OWNED_ITEM_ID = "ItemID";
-	public static final String OWNED_QUANTITY = "Quantity";
-	
+	public static final String OWNED_USER_ID = "user_id";
+	public static final String OWNED_ITEM_ID = "item_id";
+	public static final String OWNED_QUANTITY = "quantity";
+
 	// Job Table Columns names
-	public static final String JOB_ID = "ID";
-	public static final String JOB_EARNINGS = "Earnings";
-	public static final String JOB_TYPE = "Type";
-	public static final String JOB_DESCRIPTION = "Description";
-	
+	public static final String JOB_ID = "job_id";
+	public static final String JOB_EARNINGS = "earnings";
+	public static final String JOB_TYPE = "type";
+	public static final String JOB_DESCRIPTION = "description";
+
 	// create table queries here
-	public static final String CREATE_USER_TABLE = "CREATE TABLE "
-			+ TABLE_USER + "(" + USER_ID + " INTEGER PRIMARY KEY," + USER_NAME
-			+ " TEXT,"  + USER_PASSWORD	+ " TEXT," + USER_MONEY + " INTEGER" + ")";
+	public static final String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER
+			+ "(" + USER_ID + " INTEGER PRIMARY KEY," + USER_NAME + " TEXT,"
+			+ USER_PASSWORD + " TEXT," + USER_MONEY + " INTEGER" + USER_PET
+			+ " INTEGER" + ")";
 
-	public static final String CREATE_PET_TABLE = "CREATE TABLE " + TABLE_PET + "(" + PET_ID + " INTEGER PRIMARY KEY,"
-			+ PET_NAME + " TEXT, " + PET_AGE + " INTEGER, " + PET_AGE_INC + " INTEGER," + PET_WEIGHT + " DOUBLE," + PET_HAPPINESS + " INTEGER," 
-			+ PET_HEALTH + " INTEGER," + PET_HUNGER + " INTEGER," + PET_SICKNESS + " TEXT,"+ PET_MOOD + " TEXT)";
+	public static final String CREATE_PET_TABLE = "CREATE TABLE " + TABLE_PET
+			+ "(" + PET_ID + " INTEGER PRIMARY KEY," + PET_NAME + " TEXT, "
+			+ PET_AGE + " INTEGER, " + PET_AGE_INC + " INTEGER," + PET_WEIGHT
+			+ " DOUBLE," + PET_HAPPINESS + " INTEGER," + PET_HEALTH
+			+ " INTEGER," + PET_HUNGER + " INTEGER," + PET_SICKNESS + " TEXT,"
+			+ PET_MOOD + " TEXT)";
 
-	public static final String CREATE_ILLNESS_TABLE = "CREATE TABLE " + TABLE_ILLNESS + "(" + ILLNESS_NAME + " TEXT PRIMARY KEY," 
-			+ ILLNESS_HAIMPACT + " INTEGER, " + ILLNESS_HEIMPACT+ " INTEGER," + ILLNESS_HUIMPACT + " INTEGER, " 
-			+ ILLNESS_DESCRIPTION + " TEXT)";
-	
-	public static final String CREATE_MOOD_TABLE = "CREATE TABLE " + TABLE_MOOD + "(" + MOOD_TYPE + " TEXT PRIMARY KEY," + MOOD_HAIMPACT 
+	public static final String CREATE_ILLNESS_TABLE = "CREATE TABLE "
+			+ TABLE_ILLNESS + "(" + ILLNESS_NAME + " TEXT PRIMARY KEY,"
+			+ ILLNESS_HAIMPACT + " INTEGER, " + ILLNESS_HEIMPACT + " INTEGER)";
+
+	public static final String CREATE_MOOD_TABLE = "CREATE TABLE " + TABLE_MOOD
+			+ "(" + MOOD_TYPE + " TEXT PRIMARY KEY," + MOOD_HAIMPACT
 			+ " INTEGER, " + MOOD_DESCRIPTION + " TEXT)";
-	
-	public static final String CREATE_RANDEVENT_TABLE = "CREATE TABLE" + TABLE_RANDEVENT + "(" + REVENT_ID + " INTEGER PRIMARY KEY,"
-			+ REVENT_HAIMPACT + " INTEGER," + REVENT_HEIMPACT + " INTEGER," + REVENT_HUIMPACT + " INTEGER,"
-			+ REVENT_ILLNESS + " TEXT," + REVENT_ITEM + " TEXT," + REVENT_DESCRIPTION + " TEXT)";
-	
-	public static final String CREATE_EVENT_TABLE = "CREATE TABLE " + TABLE_EVENT + "(" + EVENT_PET_ID + " INTEGER, "
-			+ EVENT_RAND_ID + " INTEGER, " + EVENT_TIME + " DATETIME, PRIMARY KEY(" EVENT_PET_ID +" , " + EVENT_RAND_ID  + " )"
-			+ "FOREIGN KEY("+EVENT_PET_ID+") REFERENCES "+ TABLE_PET+"(" + PET_ID +"),  "
-			+ "FOREIGN KEY("+ EVENT_RAND_ID +") REFERENCES "+ TABLE_RANDEVENT+"(" + REVENT_ID +"))";
-	
-	public static final String CREATE_INTERACTION_TABLE = "CREATE TABLE " + TABLE_INTERACTION + "(" + INTERACTION_PET_ID 
-			+ " INTEGER PRIMARY KEY, " + INTERACTION_TIME + " DATETIME," 
-			+ INTERACTION_POST_HAPPINESS + " INTEGER," + INTERACTION_POST_HEALTH + " INTEGER,"  + INTERACTION_POST_HUNGER + " INTEGER," 
-			+ "FOREIGN KEY("+INTERACTION_PET_ID+") REFERENCES "+ TABLE_PET+"(" + PET_ID+"))";
-	
-	public static final String CREATE_ITEM_TABLE = "CREATE TABLE " + TABLE_ITEM + "("+ITEM_ID+" INTEGER PRIMARY KEY, "
-			+ ITEM_PRICE + " INTEGER," + ITEM_DESCRIPTION + " TEXT, " + ITEM_TYPE + "String, " + ITEM_IMPACT + " INTEGER, " + ITEM_ATTRIBUTE + "TEXT )";
-	
-	public static final String CREATE_OWNED_TABLE = "CREATE TABLE " + TABLE_OWNED + "(" + OWNED_USER_ID 
-			+ " INTEGER," + OWNED_ITEM_ID + " INTEGER," + OWNED_QUANTITY + " INTEGER, PRIMARY KEY( " + OWNER_USER_ID + " , " + OWNED_ITEM_ID +  " )"
-			+ "FOREIGN KEY("+ OWNED_USER_ID +") REFERENCES "+ TABLE_PET+"(" + USER_ID+"))";
-	
-	public static final String CREATE_JOB_TABLE = "CREATE TABLE " + TABLE_JOB + "(" + JOB_ID 
-			+ " INTEGER PRIMARY KEY," + JOB_EARNINGS + " INTEGER," + JOB_TYPE + " TEXT, " 
-			+ JOB_DESCRIPTION + " TEXT)";
+
+	public static final String CREATE_RANDEVENT_TABLE = "CREATE TABLE"
+			+ TABLE_RANDEVENT + "(" + REVENT_ID + " INTEGER PRIMARY KEY,"
+			+ REVENT_HAIMPACT + " INTEGER," + REVENT_HEIMPACT + " INTEGER,"
+			+ REVENT_HUIMPACT + " INTEGER," + REVENT_ILLNESS + " TEXT,"
+			+ REVENT_ITEM + " TEXT," + REVENT_DESCRIPTION + " TEXT)";
+
+	public static final String CREATE_EVENT_TABLE = "CREATE TABLE "
+			+ TABLE_EVENT + "(" + EVENT_PET_ID + " INTEGER, " + EVENT_RAND_ID
+			+ " INTEGER, " + EVENT_TIME + " DATETIME, PRIMARY KEY("
+			+ EVENT_PET_ID + " , " + EVENT_RAND_ID + " )" + "FOREIGN KEY("
+			+ EVENT_PET_ID + ") REFERENCES " + TABLE_PET + "(" + PET_ID
+			+ "),  " + "FOREIGN KEY(" + EVENT_RAND_ID + ") REFERENCES "
+			+ TABLE_RANDEVENT + "(" + REVENT_ID + "))";
+
+	public static final String CREATE_INTERACTION_TABLE = "CREATE TABLE "
+			+ TABLE_INTERACTION + "(" + INTERACTION_PET_ID
+			+ " INTEGER PRIMARY KEY, " + INTERACTION_TIME + " DATETIME,"
+			+ INTERACTION_POST_HAPPINESS + " INTEGER,"
+			+ INTERACTION_POST_HEALTH + " INTEGER," + INTERACTION_POST_HUNGER
+			+ " INTEGER," + "FOREIGN KEY(" + INTERACTION_PET_ID
+			+ ") REFERENCES " + TABLE_PET + "(" + PET_ID + "))";
+
+	public static final String CREATE_ITEM_TABLE = "CREATE TABLE " + TABLE_ITEM
+			+ "(" + ITEM_ID + " INTEGER PRIMARY KEY, " + ITEM_PRICE
+			+ " INTEGER," + ITEM_DESCRIPTION + " TEXT, " + ITEM_TYPE
+			+ "String, " + ITEM_IMPACT + " INTEGER, " + ITEM_ATTRIBUTE
+			+ "TEXT )";
+
+	public static final String CREATE_OWNED_TABLE = "CREATE TABLE "
+			+ TABLE_OWNED + "(" + OWNED_USER_ID + " INTEGER," + OWNED_ITEM_ID
+			+ " INTEGER," + OWNED_QUANTITY + " INTEGER, PRIMARY KEY( "
+			+ OWNED_USER_ID + " , " + OWNED_ITEM_ID + " )" + "FOREIGN KEY("
+			+ OWNED_USER_ID + ") REFERENCES " + TABLE_PET + "(" + USER_ID
+			+ "))";
+
+	public static final String CREATE_JOB_TABLE = "CREATE TABLE " + TABLE_JOB
+			+ "(" + JOB_ID + " INTEGER PRIMARY KEY," + JOB_EARNINGS
+			+ " INTEGER," + JOB_TYPE + " TEXT, " + JOB_DESCRIPTION + " TEXT)";
 
 	public VPDHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -157,15 +173,15 @@ public class VPDHelper extends SQLiteOpenHelper {
 		db.execSQL(CREATE_ITEM_TABLE);
 		db.execSQL(CREATE_OWNED_TABLE);
 		db.execSQL(CREATE_JOB_TABLE);
-		
+
 	}
 
 	// Upgrading database, this happens when you change the version number
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(VPDHelper.class.getName(),
-		        "Upgrading database from version " + oldVersion + " to "
-		            + newVersion + ", which will destroy all old data");
+		Log.w(VPDHelper.class.getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
 		// Drop older table if existed
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_PET);
@@ -181,8 +197,5 @@ public class VPDHelper extends SQLiteOpenHelper {
 		// Create tables again
 		onCreate(db);
 	}
-	
+
 }
-
-
-
